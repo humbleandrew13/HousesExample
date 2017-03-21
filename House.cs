616d95d91586bccd2ce77f57b2ee788f86cd719a
Andy Humble <humbleandrew13@gmail.com>
2017-03-21 05:31:46 -0400

    final

--- House.cs
+++ House.cs
@@ -8,10 +8,37 @@ namespace HousesExample
 {
     class House
     {
+        //variables
         private string color;
         private int rooms;
         private int size;
         private int doors;
+
+        private string name;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //constructors
+        public House()
+        {
+            this.color = "Gray";
+            this.rooms = 2;
+            this.size = 1000;
+            this.doors = 2;
+        }
+
+        public House(string color, int size)
+        {
+            this.color = color;
+            this.size = size;
+            this.rooms = 1;
+            this.doors = 2;
+        }
 
         public House(string color, int rooms, int size, int doors)  //constructor
         {
@@ -21,6 +48,7 @@ namespace HousesExample
             this.doors = doors;
         }
 
+        //methods
         public void Paint(string color)
         {
             this.color = color; //this.color is referring to the color variable after "private string" above
--- Program.cs
+++ Program.cs
@@ -11,8 +11,19 @@ namespace HousesExample
         static void Main(string[] args)
         {
             House house1 = new House("Green", 6, 2500, 4);
+            House house2 = new House("Gunmetal", 1200);
+            House house3 = new House();
 
-            house1.DisplayHouseInfo();
+            //house1.DisplayHouseInfo();
+            //house2.DisplayHouseInfo();
+            //house3.DisplayHouseInfo();
+
+            //house2.Paint("Burnt Sienna");
+
+            //house2.DisplayHouseInfo();
+
+            house2.Name = "The blue house";
+            Console.WriteLine(house2.Name);
         }
     }
 }
